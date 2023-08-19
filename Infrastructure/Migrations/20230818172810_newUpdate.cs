0c92c24060999e60ad03ad7b158a494f3fe2ee3e
Do1ore <100031663+Do1ore@users.noreply.github.com>
2023-08-19 19:01:30 +0300

    +RobotRover.cs multiple instance  bug fixed

---
+++ Infrastructure/Migrations/20230818172810_newUpdate.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class newUpdate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "LastLocationDateTime",
+                table: "RoverLocations",
+                newName: "LocationDateTime");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CurrentDirection",
+                table: "RoverLocations",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "LocationDateTime",
+                table: "RoverLocations",
+                newName: "LastLocationDateTime");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CurrentDirection",
+                table: "RoverLocations",
+                type: "TEXT",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER");
+        }
+    }
+}
