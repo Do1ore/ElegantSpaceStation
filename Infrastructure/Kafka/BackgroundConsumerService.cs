ffe1ad678c9aff9c27a015ed570fabe169ccec2d
Do1ore <100031663+Do1ore@users.noreply.github.com>
2023-08-20 23:44:48 +0300

    +Kafka configured

---
+++ Infrastructure/Kafka/BackgroundConsumerService.cs
@@ -0,0 +1,53 @@
+﻿using Confluent.Kafka;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace Infrastructure.Kafka;
+
+
+public class BackgoundConsumerService : BackgroundService
+{
+    private readonly KafkaConsumerService _kafkaConsumer;
+    private readonly ILogger<BackgoundConsumerService> _logger;
+
+    private readonly string _topic;
+    private readonly int _delayTime;
+    public BackgoundConsumerService(
+        KafkaConsumerService kafkaConsumer,
+        IConfiguration configuration,
+        ILogger<BackgoundConsumerService> logger
+    )
+    {
+        _kafkaConsumer = kafkaConsumer;
+
+        _topic = configuration.GetSection("Kafka")["Topic"]
+            ?? throw new ArgumentNullException("No topic found");
+
+        _delayTime = int.Parse(configuration.GetSection("BackgoundService")["DelayTimeMs"]
+            ?? throw new ArgumentNullException("No DelayTimeMs found")) - 1;
+
+        _logger = logger;
+    }
+
+    public override Task StartAsync(CancellationToken cancellationToken)
+    {
+        _logger.LogInformation("{@ServiceName} started", nameof(BackgoundConsumerService));
+        return base.StartAsync(cancellationToken);
+    }
+
+    public override Task StopAsync(CancellationToken cancellationToken)
+    {
+
+        return base.StopAsync(cancellationToken);
+    }
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        var result = _kafkaConsumer.Consume(_topic);
+        _logger.LogInformation("Consume result: {@Key}; {@Message}; {@Headers}", result.Key, result.Message, result.Headers);
+        await Task.Delay(_delayTime);
+    }
+
+
+}
---
+++ Infrastructure/Kafka/KafkaConsumerService.cs
@@ -0,0 +1,23 @@
+﻿using Confluent.Kafka;
+
+namespace Infrastructure.Kafka
+{
+    public class KafkaConsumerService
+    {
+        private readonly IConsumer<Ignore, string> _consumer;
+
+        public KafkaConsumerService(ConsumerConfig consumerConfig)
+        {
+            _consumer = new ConsumerBuilder<Ignore, string>(consumerConfig).Build();
+        }
+
+        public ConsumeResult<Ignore, string> Consume(string topic)
+        {
+            _consumer.Subscribe(topic);
+            var consumeResult = _consumer.Consume();
+
+            return consumeResult;
+
+        }
+    }
+}
